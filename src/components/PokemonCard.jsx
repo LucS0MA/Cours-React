f3d526f0900c25da090152d726b3d1fe97067a4b
Lucien KOHLI <lucien.kohli@gmail.com>
2023-12-11 22:09:49 +0100

    Props.1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,10 @@
 const PokemonCard = (props) => {
-    const pokemon = props.pokemon;
-    console.log(props)
+    console.log(props);
+
   return (
     <figure>
-        {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
-            <figcaption>{pokemon.name}</figcaption>
+        {props.pokemon.imgSrc ? (<img src={props.pokemon.imgSrc} alt={props.pokemon.name} />) : (<p>???</p>)}
+            <figcaption>{props.pokemon.name}</figcaption>
     </figure>
   );
 };
